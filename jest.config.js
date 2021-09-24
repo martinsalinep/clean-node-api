22087333c17b4c887e110c58bf0aead00508423b
martinsalinep <alinemartins@hotmail.com.br>
2021-09-24 08:42:18 -0300

    ensure loginRouter returns a custom error if no email or password are provided

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  coverageProvider: "v8",
-};
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
+  preset: '@shelf/jest-mongodb'
+}
